4b833333a6998b842fe8ac395d9c6d942a841356
hwan2-99 <thh1233123@gmail.com>
2023-11-29 18:16:16 +0900

    feat : 추천 컨트롤러 실행시 메뉴 컨트롤러 초기화 구현

--- src/main/java/menu/controller/RecommendController.java
+++ src/main/java/menu/controller/RecommendController.java
@@ -5,6 +5,11 @@ import java.util.List;
 import menu.service.InputService;
 
 public class RecommendController {
+    private final MenuController menuController = new MenuController();
+    public RecommendController() {
+        menuController.run();
+    }
+
     private final InputService inputService = new InputService();
 
     public void run() {
